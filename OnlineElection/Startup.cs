dfe75107202d13a73fa330df151849d667ff2176
amilaDulanjana <amiladulanjana@hotmail.com>
2016-09-16 11:07:16 +0530

    Add project files.

---
+++ OnlineElection/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineElection.Startup))]
+namespace OnlineElection
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
