4ad97884f0a4a640becd6dc8876505b62b572ff0
amilaDulanjana <amiladulanjana@hotmail.com>
2016-09-16 22:26:36 +0530

    Repository classes added

---
+++ OnlineElection.BLL/Repository/PersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineElection.BLL.Repository
+{
+    class PersonRepository : IPersonRepository
+    {
+        public void InsertCandidate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
