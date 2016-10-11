94cb5ac8f52ffec3d0f731097363915512027dba
amilaDulanjana <amiladulanjana@hotmail.com>
2016-10-11 22:41:38 +0530

    view user file created

--- OnlineElection/Controllers/Admin/UserController.cs
+++ OnlineElection/Controllers/Admin/UserController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlineElection.DAL;
+using OnlineElection.BLL.Repository;
 
 namespace AdminPanel.Controllers
 {
     public class UserController : Controller
     {
+        PersonRepository repository = new PersonRepository();
         // GET: User
         //public ActionResult Index()
         //{
@@ -16,8 +19,13 @@ namespace AdminPanel.Controllers
 
         public ActionResult AddUser()
         {
+            person person = new person();
+            return View("~/Views/AdminViews/User/AddUser.cshtml");
+        }
 
-            return View();
+        public ActionResult ViewUsers()
+        {
+            return View("~/Views/AdminViews/User/ViewUsers.cshtml");
         }
     }
 }
