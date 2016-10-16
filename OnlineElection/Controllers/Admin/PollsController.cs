3ff3348f92fca05095389c486e10e61426b498c5
amilaDulanjana <amiladulanjana@hotmail.com>
2016-10-16 22:49:47 +0530

    polls

--- OnlineElection/Controllers/Admin/PollsController.cs
+++ OnlineElection/Controllers/Admin/PollsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineElection.BLL.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace OnlineElection.Controllers.Admin
 {
     public class PollsController : Controller
     {
+        PersonRepository repository = new PersonRepository();
         // GET: Polls
         public ActionResult Index()
         {
@@ -17,6 +19,11 @@ namespace OnlineElection.Controllers.Admin
         public ActionResult CreatePoll()
         {
             return View("~/ViewsAdmin/Polls/CreatePoll.cshtml");
+        }
+
+        public ActionResult GetCandidates()
+        {
+            return PartialView("~/ViewsAdmin/Polls/_GetCandidates.cshtml", repository.GetAll());
         }
     }
 }
