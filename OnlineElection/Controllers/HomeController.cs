5d00bb6c6bd4eea06856914026614292e024f783
kavindasas <kaviliya123@gmail.com>
2016-10-26 18:52:13 +0530

    User toast notification
    Added for the homepage only

--- OnlineElection/Controllers/HomeController.cs
+++ OnlineElection/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineElection.BLL.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace OnlineElection.Controllers
 {
     public class HomeController : Controller
     {
+        MessegeRepository _messageRepository = new MessegeRepository();
+
         public ActionResult Index()
         {
             return View();
@@ -26,5 +29,23 @@ namespace OnlineElection.Controllers
 
             return View();
         }
+
+
+
+
+        public ActionResult GetMessages()
+        {
+            string user = "21781DD9-4A1C-4DFB-933F-21633B94B61B";
+
+            return PartialView("~/ViewsAdmin/Shared/_MessagesList.cshtml", _messageRepository.GetAllMessages(user));
+        }
+
+        public ActionResult GetNotifications()
+        {
+            string user = "21781DD9-4A1C-4DFB-933F-21633B94B61B";
+
+            return PartialView("~/ViewsAdmin/Shared/_MessagesListNotifications.cshtml", _messageRepository.GetAllMessagesNew(user));
+        }
+
     }
 }
